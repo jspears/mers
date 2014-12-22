39ca22c4e3b32899fcb3cea71e37820121a28116
jspears <speajus@gmail.com>
2014-12-21 19:15:49 -0800

    started refactor

--- test/when-mocha.js
+++ test/when-mocha.js
@@ -7,15 +7,15 @@ function resolve(promise, val, timeout) {
 }
 function resolveErr(promise, val, timeout) {
     setTimeout(function () {
-        promise.resolve( val);
+        promise.resolve(val);
     }, timeout);
 }
-describe('when all resolved', function () {
+describe.only('when all resolved', function () {
 
     it('should resolve a promise when all children have been resolved', function (done) {
 
         var p1 = new Promise(), p2 = new Promise(), p3 = new Promise();
-        util.when([p1, p2, p3]).then(function ( ret) {
+        util.when([p1, p2, p3]).then(function (ret) {
             console.log('ret', ret);
             ret.should.have.lengthOf(3);
             done();
@@ -28,7 +28,7 @@ describe('when all resolved', function () {
 
         var p1 = new Promise(), p2 = new Promise(), p3 = new Promise();
         p2.resolve(null, 2);
-        util.when([p1, p2, p3]).then(function ( ret) {
+        util.when([p1, p2, p3]).then(function (ret) {
             console.log('ret', ret);
             ret.should.have.lengthOf(3);
             done();
@@ -42,7 +42,7 @@ describe('when all resolved', function () {
         p2.resolve(null, 2);
         p3.resolve(null, 3);
         p1.resolve(null, 1);
-        util.when([p1, p2, p3]).then(function ( ret) {
+        util.when([p1, p2, p3]).then(function (ret) {
             console.log('ret', ret);
             ret.should.have.lengthOf(3);
             done();
@@ -52,7 +52,7 @@ describe('when all resolved', function () {
 
         var p1 = new Promise();
         p1.resolve(null, 1);
-        util.when([p1]).then(function ( ret) {
+        util.when([p1]).then(function (ret) {
             console.log('ret', ret);
             ret.should.have.lengthOf(1);
             done();
@@ -62,7 +62,7 @@ describe('when all resolved', function () {
 
         var p1 = new Promise();
         p1.resolve(1);
-        util.when([p1]).then(null, function(ret){
+        util.when([p1]).then(null, function (ret) {
             ret.should.have.lengthOf(1);
             done();
         });
@@ -71,12 +71,32 @@ describe('when all resolved', function () {
     it('should short circuit on error', function (done) {
 
         var p1 = new Promise(), p2 = new Promise(), p3 = new Promise();
-        util.when([p1, p2, p3]).then(function ( ret) {
-        }, function(e){
+        util.when([p1, p2, p3]).then(function (ret) {
+        }, function (e) {
             done();
         });
         resolveErr(p1, 1, 300);
         resolve(p2, 2, 100);
         resolve(p3, 3, 200);
     });
+    it('should resolve the value when it is not a promise', function (done) {
+        var p1 = new Promise(), p2 = new Promise(), p3 = 3;
+        util.when(p1, p2, p3).then(function (values) {
+            values.should.have.property(0, 1);
+            values.should.have.property(1, 2);
+            values.should.have.property(2, 3);
+            done();
+        });
+        resolve(p1, 1, 300);
+        resolve(p2, 2, 100);
+    })
+    it('should reject the value when the value is an error', function (done) {
+        var p1 = new Promise(), p2 = new Promise(), p3 = new Error();
+        util.when(p1, p2, p3).then(done, function (e) {
+            done();
+        });
+        resolve(p1, 1, 300);
+        resolve(p2, 2, 100);
+    })
+
 });
