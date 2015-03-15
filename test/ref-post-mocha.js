2f85c066839a41ce046b07e2e6cde19a7d1377c0
jspears <speajus@gmail.com>
2015-03-15 16:16:01 -0700

    fixed

--- test/ref-post-mocha.js
+++ test/ref-post-mocha.js
@@ -83,7 +83,6 @@ describe('testing nested refs', function () {
 
     });
     it('should post to group employee ref array', function (done) {
-        console.log('finding ' + group._id);
         request(app)
             .post('/rest/Group/' + group._id + '/employees')
             .set('Content-Type', 'application/json')
